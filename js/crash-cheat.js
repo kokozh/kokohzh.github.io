031ed6d8550d58ad6ba53788286ef6548606c9c1
koko君 <1213880299@qq.com>
2019-11-17 21:43:04 +0800

    Site updated: 2019-11-17 21:43:01

--- js/crash-cheat.js
+++ js/crash-cheat.js
@@ -2,11 +2,11 @@ var oldTitle = document.title;
 var titleTime; //標題恢復計時器
 document.addEventListener("visibilitychange", function () {
   if (document.hidden) {
-    document.querySelector("[rel='icon']").setAttribute("href", "https://i.loli.net/2018/08/24/5b7fcb00ed9bf.png");
-    document.title = "網站崩潰了！";
+    document.querySelector("[rel='icon']").setAttribute("href", "1.png");
+    document.title = "ko等你回来！";
     clearTimeout(titleTime);
   } else {
-    document.title = "其實並沒有！";
+    document.title = "欢迎回来！";
     document.querySelector("[rel='icon']").setAttribute("href", "/images/apple-touch-icon-next.png");
     titleTime = setTimeout(function () {
       document.title = oldTitle;
